9f2c2ffcaa0f70105f53cd98b94dd4077b918dd0
korynewton <korynewtondev@gmail.com>
2019-04-12 09:16:57 -0700

    Games model tests

---
+++ games/gamesModel.spec.js
@@ -0,0 +1,25 @@
+const db = require('../data/dbConfig');
+
+const Games = require('./gamesModel');
+
+describe('Games model testing', () => {
+  beforeEach(async () => {
+    await db('games').truncate();
+  });
+  it('insert() tests', async () => {
+    await Games.insert({ title: 'Call of Duty', genre: '1st person shooter' });
+    await Games.insert({ title: 'Starfox', genre: 'N64 Classic' });
+
+    const games = await db('games');
+
+    expect(games).toHaveLength(2);
+  });
+
+  it('should insert the correct game', () => {
+      let game = await Games.inser({ title:'Super Smash Bros', genre:"fighting" })
+      expect(game.title).toBe('Super Smash Bros');
+      
+      game = await Games.insert({ title:'Fifa', genre:'Sports' })
+      expect(game.title).toBe('Fifa')
+  });
+});
